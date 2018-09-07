b666ed7e24e5efc50eb9fe752061b50176e1edda
Lam Le V <lam.le@asiantech.vn>
2018-09-07 16:35:14 +0700

    Define Api paths

---
+++ UnittestExample/Model/API/API.Categories.swift
@@ -0,0 +1,30 @@
+//
+//  API.Categories.swift
+//  UnittestExample
+//
+//  Created by Quang Phu C. M. on 9/7/18.
+//  Copyright © 2018 IOS Testing. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
+extension Api.Categories {
+    struct CategoryListParams {
+        static var JSON: [String: Any] {
+            return ["part": "snippet",
+                    "key": App.Key.youtubeAPIKey,
+                    "regionCode": regionCode]
+        }
+    }
+
+    @discardableResult
+    static func getCategoryList(completion: @escaping Completion) -> Request? {
+        let path = Api.Path.Categories.path
+        return api.request(method: .get, urlString: path, parameters: CategoryListParams.JSON, completion: { (result) in
+            DispatchQueue.main.async {
+                // TODO: - Pls mapper object and get `channelID` with format example `CategoryList.json`
+            }
+        })
+    }
+}
---
+++ UnittestExample/Model/API/API.Video.swift
@@ -0,0 +1,32 @@
+//
+//  API.Video.swift
+//  UnittestExample
+//
+//  Created by Quang Phu C. M. on 9/7/18.
+//  Copyright © 2018 IOS Testing. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
+extension Api.Video {
+    struct VideoListParams {
+        let playlistID : String
+
+        var JSON: [String: Any] {
+            return ["part": "snippet",
+                    "key": App.Key.youtubeAPIKey,
+                    "playlistId": playlistID]
+        }
+    }
+
+    @discardableResult
+    static func getVideoList(param: VideoListParams, completion: @escaping Completion) -> Request? {
+        let path = Api.Path.Video.path
+        return api.request(method: .get, urlString: path, parameters: param.JSON, completion: { (result) in
+            DispatchQueue.main.async {
+                // TODO: - Pls mapper video object with format example `Video.json`
+            }
+        })
+    }
+}
