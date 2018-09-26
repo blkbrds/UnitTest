38bf8370e5e214c4ce4959c317c8f6d9f008ae1f
Khoa Nguyen D.B <khoa.nguyen@asiantech.vn>
2018-09-26 13:35:44 +0700

    Test API Categories

---
+++ UnittestExampleTests/ViewModel/Api/CategoriesTests.swift
@@ -0,0 +1,41 @@
+//
+//  Categories.swift
+//  UnittestExampleTests
+//
+//  Created by MBA0219 on 9/25/18.
+//  Copyright © 2018 IOS Testing. All rights reserved.
+//
+
+import Quick
+import Nimble
+import Alamofire
+import Mockingjay
+
+@testable import UnittestExample
+
+class ApiCategoriesTests: QuickSpec {
+
+    override func spec() {
+        describe("Api Categories test") {
+            it("Success") {
+                let data = Data(forResource: "CategoryList", ofType: "json", on: self)
+                let path = Api.Path.Categories.path
+                self.stub(http(.get, uri: path, parameters: Api.Categories.CategoryListParams.JSON), jsonData(data))
+                waitUntil(timeout: 20, action: { done in
+                    Api.Categories.getCategoryList(completion: { (result) in
+                        switch result {
+                        case .success(let value):
+                            if let value = value as? Categories {
+                                expect(value.kind) == "youtube#videoCategoryListResponse"
+                                expect(value.etag) == "\"XI7nbFXulYBIpL0ayR_gDh3eu1k/1v2mrzYSYG6onNLt2qTj13hkQZk\""
+                            }
+                        case .failure(let error):
+                            fail(error.localizedDescription)
+                        }
+                        done()
+                    })
+                })
+            }
+        }
+    }
+}
