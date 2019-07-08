39cc6c84618c8d36f4a01d9f239eb014259a372a
Lam Le V <lam.le@asiantech.vn>
2019-07-08 10:08:53 +0700

    Init gemfile and travis

---
+++ UnittestExample/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  UnitTestExample
+//
+//  Created by Lam Le V. on 7/7/19.
+//  Copyright © 2019 Lam Le V. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
