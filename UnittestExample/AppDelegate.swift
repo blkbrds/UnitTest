c649bb51be09d636a9a4d18d84715786247172f5
Lam Le V <lam.le@asiantech.vn>
2019-07-09 23:06:06 +0700

    Testing with scheme

--- UnittestExample/AppDelegate.swift
+++ UnittestExample/AppDelegate.swift
@@ -15,7 +15,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         configWindow()
+        #if DEBUG
+        fatalError()
+        #else
         return true
+        #endif
     }
 
     private func configWindow() {
@@ -25,4 +29,3 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         window.makeKeyAndVisible()
     }
 }
-
