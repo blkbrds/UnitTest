2111f962e61bccf326aa70b1be35fef30509d340
Lam Le V <lam.le@asiantech.vn>
2019-09-02 23:12:27 +0700

    Change add-key.sh

--- scripts/add-key.sh
+++ scripts/add-key.sh
@@ -1,22 +1,22 @@
-#!/bin/bash
-
-ios-build.keychain=ios-build.keychain
+#!/bin/sh
+# Create a custom keychain
 security create-keychain -p travis ios-build.keychain
-# Make the keychain the default so identities are found
+
+# Make the custom keychain default, so xcodebuild will use it for signing
 security default-keychain -s ios-build.keychain
+
 # Unlock the keychain
 security unlock-keychain -p travis ios-build.keychain
-# Set keychain locking timeout to 3600 seconds
-security set-keychain-settings -t 3600 -u ios-build.keychain
+
+# Set keychain timeout to 1 hour for long builds
+# see http://www.egeek.me/2013/02/23/jenkins-and-xcode-user-interaction-is-not-allowed/
+security set-keychain-settings -t 3600 -l ~/Library/Keychains/ios-build.keychain
 
 # Add certificates to keychain and allow codesign to access them
-security import ./Cers/dis.cer -k ios-build.keychain -T /usr/bin/codesign
-security import ./Cers/dev.cer -k ios-build.keychain -T /usr/bin/codesign
+security import ./Cers/dev.p12 -k ~/Library/Keychains/ios-build.keychain -P 12345678 -T /usr/bin/codesign
+security import ./Cers/dis.p12 -k ~/Library/Keychains/ios-build.keychain -P 12345678 -T /usr/bin/codesign
 
-security import ./Cers/dis.p12 -k ios-build.keychain -P 12345678  -T /usr/bin/codesign
-security import ./Cers/dev.p12 -k ios-build.keychain -P 12345678  -T /usr/bin/codesign
-
-security set-key-partition-list -S apple-tool:,apple: -s -k travis ~/Library/Keychains/ios-build.keychain
+security set-key-partition-list -S apple-tool:,apple: -s -k travis ios-build.keychain
 
 echo "list keychains: "
 security list-keychains
@@ -34,10 +34,3 @@ cp ./Cers/Fastlane_dev.mobileprovision ~/Library/MobileDevice/Provisioning\ Prof
 
 uuid=`grep UUID -A1 -a ./Cers/TheLastProject_AdHoc.mobileprovision | grep -io "[-A-F0-9]\{36\}"`
 cp ./Cers/TheLastProject_AdHoc.mobileprovision ~/Library/MobileDevice/Provisioning\ Profiles/$uuid.mobileprovision
-
-#cp ./scripts/profiles/TheLastProject_Dev.mobileprovision ~/Library/MobileDevice/Provisioning\ Profiles/
-#cp ./scripts/profiles/TheLastProject_AdHoc.mobileprovision ~/Library/MobileDevice/Provisioning\ Profiles/
-
-echo "show provisioning profile"
-ls ~/Library/MobileDevice/Provisioning\ Profiles
-echo " ****** "
