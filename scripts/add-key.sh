5c7b2c397c8be6f9ec6774e58598ee5a6a6a05e7
Lam Le V <lam.le@asiantech.vn>
2019-09-02 17:39:39 +0700

    Change password

--- scripts/add-key.sh
+++ scripts/add-key.sh
@@ -13,6 +13,6 @@ security unlock-keychain -p travis ios-build.keychain
 security set-keychain-settings -t 3600 -l ~/Library/Keychains/ios-build.keychain
 
 # Add certificates to keychain and allow codesign to access them
-security import ./Cers/Dev.p12 -k ~/Library/Keychains/ios-build.keychain -P 12345678 -T /usr/bin/codesign
+security import ./Cers/Dev.p12 -k ~/Library/Keychains/ios-build.keychain -P "12345678" -T /usr/bin/codesign
 
 security set-key-partition-list -S apple-tool:,apple: -s -k travis ios-build.keychain
